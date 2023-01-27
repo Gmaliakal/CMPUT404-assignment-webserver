50c6b9e3c67b63ed6dca0c21e2cbe63d10f638ba
Georgin M <Georgin@Eliza-MBP.ed.shawcable.net>
2023-01-26 20:32:36 -0700

    added screen shots and licensing

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import socketserver
 import os
 from os import path
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2023 Abram Hindle, Eddie Antonio Santos, Georgin Maliakal
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
